547898d35c4e4ba2f18ecf5d1be769137493fa92
GitHubのユーザー名 <19t0829b@gmail.com>
2025-08-11 13:01:35 +0900

    fix kadai_sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -13,31 +13,25 @@
         $nums = [15, 4, 18, 23, 10 ];
 
         function sort_2way($array, $order) {
-        if ($order === TRUE) {
-            sort($array); // 昇順
-        } else {
-            rsort($array); // 降順
-        }
-        return $array; // 並び替えた配列を返す
-
+            if ($order === TRUE) {
+                echo '昇順にソートします。<br>';
+                sort($array); // 昇順
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($array); // 降順
+            }
+        
+            foreach ($array as $num) {
+                echo $num . "<br>";
+            }
         }
     
         // ソート順の指定（TRUE:昇順, FALSE:降順）
-        $order = FALSE; // または FALSE
+        $order = TRUE; 
+        $Asc= sort_2way($nums, $order);
 
-        if ($order === TRUE) {
-            echo '昇順にソートします。<br>';
-        } elseif ($order === FALSE) {
-            echo '降順にソートします。<br>';
-        }
-
-        $result = sort_2way($nums, $order);
-
-        foreach ($result as $num) {
-            echo $num . "<br>";
-        }
-
-
+        $order = FALSE; 
+        $Des = sort_2way($nums, $order);    
 
         ?>
     </p>
