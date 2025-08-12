c0d38813d12e0fbf1cc632f750c07436c2cd9b88
GitHubのユーザー名 <19t0829b@gmail.com>
2025-08-12 14:25:20 +0900

    add kadai_016

---
+++ kadai_016/class.php
@@ -0,0 +1,76 @@
+<!--======== 前略 ========-->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // クラスを定義する
+        class Food {
+            // プロパティを定義する                        
+            private $name;
+            private $price;
+
+            // コンストラクタを定義する
+            public function __construct(string $name, int $price) {
+                $this->name = $name; 
+                $this->price = $price;
+            }
+            
+            //メゾッドの定義
+            function show_price () {
+                echo $this->price .  '<br>';
+            }
+        }
+
+        // クラスを定義する
+        class Animal {
+            // プロパティを定義する                        
+            private $name;
+            private $height;
+            private $weight;
+
+            // コンストラクタを定義する
+            public function __construct(string $name, int $height, string $weight) {
+                $this->name = $name; 
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+            
+            //メゾッドの定義
+            function show_height () {
+                echo $this->height .  '<br>';
+            }
+        }
+
+        //オブジェクトを定義する
+        $potato = new Food (
+            'potate',
+            250
+        );
+
+        $dog = new Animal (
+            'dog',
+            60,
+            5000
+        );
+
+        //出力する
+        print_r($potato);
+        echo '<br>';
+        print_r($dog);
+        echo '<br>';
+        $potato->show_price();
+        $dog->show_height();
+
+        ?>
+    </p>
+
+</body>
+
+</html>
